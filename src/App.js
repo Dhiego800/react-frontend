06d078858d1beeef34be1176b8402310c4531e70
unknown <diiego.mm@hotmail.com>
2020-06-04 15:25:11 -0300

    configuring redux [Ok]

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from 'react';
-import Routes from './routes'
+import { Provider } from 'react-redux';
 
-const App = () => <Routes />
+import store from './store'
+import Routes from './routes';
+
+const App = () => (
+  <Provider store={store}>
+    <Routes />
+  </Provider>
+)
 
 export default App;
